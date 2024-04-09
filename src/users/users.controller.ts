1b26fabfb65ade7082313919c6dc390388f1f23d
Florentin Portets <florentin.portets@ynov.com>
2024-04-09 13:38:56 +0200

    Flo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,8 +16,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { AuthService } from './auth/auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
-import { Public} from './auth/auth.guard';
-import  {Request as ExpressRequest} from 'express'
+import { Public } from './auth/auth.guard';
+import { Request as ExpressRequest } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -33,15 +33,15 @@ export class UsersController {
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
-  
+
   //Route de login
   @Public()
   @Post('auth/login')
   async login(@Body() dto: LoginUserDto): Promise<{ access_token: string }> {
-    const access_token = await this.authservice.signIn(dto)
-    return { access_token }
+    const access_token = await this.authservice.signIn(dto);
+    return { access_token };
   }
-  
+
   @Get()
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
@@ -51,11 +51,11 @@ export class UsersController {
   async getMyUserInfo(@Request() req: ExpressRequest): Promise<User> {
     return req.user as User;
   }
-  
+
   @Get(':id')
-  async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<User>{
-    const user= await this.usersService.getUserInfo(id);
-    if(!user) throw new NotFoundException();
+  async findOne(@Param('id', ParseUUIDPipe) id: string): Promise<User> {
+    const user = await this.usersService.getUserInfo(id);
+    if (!user) throw new NotFoundException();
     return user;
   }
 }
