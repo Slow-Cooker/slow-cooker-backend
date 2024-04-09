33621efddc10ea6040160975d04492a3807909e8
Lora Seguin <lora80123@gmail.com>
2024-04-09 17:45:42 +0200

    create fonctions of route

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { UpdateRecipeDto } from './dto/update-recipe.dto';
+import { Recipe } from './entities/recipe.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RecipeService {
-  create(createRecipeDto: CreateRecipeDto) {
-    return 'This action adds a new recipe';
+  constructor(
+    @InjectRepository(Recipe)
+    private readonly recipeRepository: Repository<Recipe>,
+  ){}
+
+  async create(createRecipeDto: CreateRecipeDto) {
+    const newRecipe = this.recipeRepository.create(createRecipeDto);
+    const saveRecipe = await this.recipeRepository.save(newRecipe);
+    return saveRecipe;
   }
 
-  findAll() {
-    return `This action returns all recipe`;
+  async findAll() {
+    const allRecipe = await this.recipeRepository.find()
+    return allRecipe;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} recipe`;
+  async findOne(id: string) {
+    const recipe = await this.recipeRepository.findOne({
+      where: {
+        id_recipe: id
+      }
+    })
+    return recipe;
   }
 
-  update(id: number, updateRecipeDto: UpdateRecipeDto) {
-    return `This action updates a #${id} recipe`;
+  async update(id: string, updateRecipeDto: UpdateRecipeDto) {
+    const recipe = await this.recipeRepository.findOneBy({ id_recipe:id });
+    if(!recipe){
+      throw new NotFoundException("This recipe dosen\'t exist")
+    }
+    const updatedRecipe = this.recipeRepository.merge(recipe, updateRecipeDto);
+    const recipeSave = await this.recipeRepository.save(updatedRecipe);
+    return recipeSave;
   }
 
-  remove(id: number) {
+  async remove(id: string) {
+    const recipe = await this.recipeRepository.findOneBy({ id_recipe:id });
+    if(!recipe){
+      throw new NotFoundException("This recipe dosen\'t exist")
+    }
+    this.recipeRepository.delete(recipe.id_recipe)
     return `This action removes a #${id} recipe`;
   }
 }
