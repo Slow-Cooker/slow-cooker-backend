725bb0b25b2073c7c2c73e345fbe84d8c2315737
Lora Seguin <lora80123@gmail.com>
2024-04-09 17:44:31 +0200

    update updateingredientdto

--- src/ingredient/dto/update-ingredient.dto.ts
+++ src/ingredient/dto/update-ingredient.dto.ts
@@ -1,7 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateIngredientDto } from './create-ingredient.dto';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateIngredientDto extends PartialType(CreateIngredientDto) {
+    @IsOptional()
+    @IsString()
     name_ingredient?: string;
+
+    @IsOptional()
     image_ingredient?: string;
 }
