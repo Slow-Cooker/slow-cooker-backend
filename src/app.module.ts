19c716c98cd1305b68f14247dec681d0a0779cf5
Lora Seguin <lora80123@gmail.com>
2024-04-09 14:51:26 +0200

    change config of file, add connection to database and view with swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Ingredient } from './ingredient/entities/ingredient.entity';
+import { IngredientModule } from './ingredient/ingredient.module';
+import { RecipeModule } from './recipe/recipe.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST', 'localhost'),
+        port: configService.get<number>('DATABASE_PORT', 5432),
+        username: configService.get<string>('DATABASE_USERNAME', 'postgres'),
+        password: configService.get<string>('DATABASE_PASSWORD', 'postgres'),
+        database: configService.get<string>('DATABASE_NAME', 'slow-cooker'),
+        entities: [Ingredient],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    IngredientModule,
+    RecipeModule,
+  ],
+  providers: [ConfigService],
 })
 export class AppModule {}
