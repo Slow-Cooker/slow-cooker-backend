23c13dbde4110e39c55093d8539d186313673308
Lora Seguin <lora80123@gmail.com>
2024-04-09 17:44:10 +0200

    update createingredientdto

--- src/ingredient/dto/create-ingredient.dto.ts
+++ src/ingredient/dto/create-ingredient.dto.ts
@@ -1,4 +1,12 @@
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateIngredientDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
     name_ingredient: string;
+
+    @IsNotEmpty()
+    @IsString()
     image_ingredient: string;
 }
