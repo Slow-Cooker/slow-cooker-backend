031df81373333fb1a5b1d2c6898347a3b87fd0fd
eliot guihal <eliot.guihal@ynov.com>
2024-05-06 09:35:43 +0100

    Fix eslint errors and add famousrecipe route

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { CreateLikeDto } from './dto/create-like.dto';
 
-@Controller(':recipeId/likes')
+@Controller()
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
 
-  @Post()
+  @Post(':recipeId/likes')
   create(
     @Param('recipeId') recipeId: string,
     @Body() createLikeDto: CreateLikeDto,
@@ -14,17 +14,22 @@ export class LikeController {
     return this.likeService.create(recipeId, createLikeDto);
   }
 
-  @Get()
+  @Get(':recipeId/likes')
   findAll(@Param('recipeId') recipeId: string) {
     return this.likeService.findAll(recipeId);
   }
 
-  @Get('/likeduser')
+  @Get('likes')
+  getFamousRecipes() {
+    return this.likeService.findFamousRecipe();
+  }
+
+  @Get(':recipeId/likes/likeduser')
   findAlluserlike(@Param('recipeId') recipeId: string) {
     return this.likeService.findAlluserlike(recipeId);
   }
 
-  @Delete(':likeId')
+  @Delete(':recipeId/likes:likeId')
   remove(@Param('recipeId') recipeId: string, @Param('likeId') likeId: string) {
     return this.likeService.remove(recipeId, likeId);
   }
